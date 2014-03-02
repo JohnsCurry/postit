80d8fde567fbebd05f17853c630686dfc4053443
John S Curry <john.steven.curry@gmail.com>
2014-03-02 12:05:10 -0700

    added description field in post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(params.require(:comment).permit(:body))
+
+    if @comment.save
+      flash[:notice] = "Your comment was added"
+      redirect_to post_path(@post)
+    else
+      render 
+    end
+
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def new
@@ -24,12 +27,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       flash[:notice] = "Post was updated"
       redirect_to posts_path
@@ -42,6 +42,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit!
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 
