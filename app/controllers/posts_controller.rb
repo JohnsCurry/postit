9b07120febeeca505042ee4c72f5d5072e6fe0cb
John S Curry <john.steven.curry@gmail.com>
2014-03-01 21:22:33 -0700

    working forms, but need username

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,15 +18,24 @@ class PostsController < ApplicationController
       flash[:notice] = "Your post has been saved!"
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
 
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Post was updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   private
