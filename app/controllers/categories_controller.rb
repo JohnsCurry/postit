6c59bc90f0484d1f739677a3d5e84eda5c88cd41
John S Curry <john.steven.curry@gmail.com>
2014-03-03 13:29:01 -0700

    new category and styling, has bug showing breadcrumbs twice

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,24 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(post_params)
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Your category has been added"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
