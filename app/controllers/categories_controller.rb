f906a010adcfe94efff5d5fd1a853ad2b792a7f9
John S Curry <john.steven.curry@gmail.com>
2014-03-02 20:52:55 -0700

    category route added, and view but not finished

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,15 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(post_params)
+  end
+
+end
