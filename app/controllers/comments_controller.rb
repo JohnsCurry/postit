9426647c51057d71ba7c24a3cbe5ca31834e48b3
John S Curry <john.steven.curry@gmail.com>
2014-03-04 09:31:25 -0700

    finished show comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(params.require(:comment).permit(:body))
-
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
+    @comment.creator = User.first
     if @comment.save
       flash[:notice] = "Your comment was added"
       redirect_to post_path(@post)
     else
-      render 
+      render 'posts/show'
     end
 
   end
