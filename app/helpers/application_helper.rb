aff9aa0249d17c53a71b7ef197b234ae8e69cc49
John S Curry <john.steven.curry@gmail.com>
2014-03-04 22:17:31 -0700

    everything seems to be working except showing two breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def fix_url(str)
     str.starts_with?('http://') ? str : "http://#{str}"
   end
+
+  def display_datetime(time)
+    time.strftime("%m/%d/%Y 1:%M%P %Z")
+  end
 end
